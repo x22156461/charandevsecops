3fab7957a28572024a13d683fed2db2657eb566b
EC2 Default User <ec2-user@ip-172-31-5-108.eu-west-1.compute.internal>
2023-12-12 17:40:09 +0000

    adding elb config

--- InventorySystem/settings.py
+++ InventorySystem/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'InventorySystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '8IPkvMwMKn6PqkJCvXme',
+        'HOST': 'devsecopstry6.chwlezgyi7rm.eu-west-1.rds.amazonaws.com',  
+        'PORT': '5432',       
     }
 }
+
 
 
 # Password validation
